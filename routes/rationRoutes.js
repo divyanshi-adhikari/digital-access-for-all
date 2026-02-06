e1935896b4d355bfaea714aafd6240d0489f631a
divyanshi-adhikari <divyanshiadhikari11@gmail.com>
2026-02-06 14:29:08 +0530

    Remove old backend folders

--- routes/rationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller =  = require("../controllers/rationController");
-
-router.post("/ration", controller.createRation);
-router.get("/ration", controller.getRation);
-
-module.exports = router;
-
